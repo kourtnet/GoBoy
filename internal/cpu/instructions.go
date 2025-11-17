7cb4b47207bbba03dbc48b1c00d422d1d95ab189
kourtnet <itromart@gmail.com>
2025-11-17 10:34:20 +0300

    LD_8bit: moved register determination into a separate func

--- internal/cpu/instructions.go
+++ internal/cpu/instructions.go
@@ -1,70 +1,6 @@
 package cpu
 
-func (cpu *CPU) nop() {
-}
-
-// TODO: ADD FLAGS AND OUT OF RANGE ARITHMETICS
-func (cpu *CPU) incA() {
-	cpu.registers.A++
-}
-
-// TODO: ADD FLAGS AND OUT OF RANGE ARITHMETICS
-func (cpu *CPU) incPC() {
-	cpu.registers.PC++
-}
-
-func (cpu *CPU) ldR8R8(r1Name, r2Name byte) func() {
-	if r1Name == r2Name {
-		return func() {}
-	}
-
-	var r1, r2 *byte
-
-	switch r1Name {
-	case 'A':
-		r1 = &cpu.registers.A
-	case 'B':
-		r1 = &cpu.registers.B
-	case 'C':
-		r1 = &cpu.registers.C
-	case 'D':
-		r1 = &cpu.registers.D
-	case 'E':
-		r1 = &cpu.registers.E
-	case 'H':
-		r1 = &cpu.registers.H
-	case 'L':
-		r1 = &cpu.registers.L
-	default:
-		panic("unknown register name for r1")
-	}
-
-	switch r2Name {
-	case 'A':
-		r2 = &cpu.registers.A
-	case 'B':
-		r2 = &cpu.registers.B
-	case 'C':
-		r2 = &cpu.registers.C
-	case 'D':
-		r2 = &cpu.registers.D
-	case 'E':
-		r2 = &cpu.registers.E
-	case 'H':
-		r2 = &cpu.registers.H
-	case 'L':
-		r2 = &cpu.registers.L
-	default:
-		panic("unknown register name for r2")
-	}
-
-	return func() {
-		*r1 = *r2
-	}
-}
-
-// Requires fetchData as first procedure in instruction
-func (cpu *CPU) ldR8N8(rName byte) func() {
+func (cpu *CPU) determineReg(rName byte) *byte {
 	var r *byte
 
 	switch rName {
@@ -85,6 +21,39 @@ func (cpu *CPU) ldR8N8(rName byte) func() {
 	default:
 		panic("unknown register name for r2")
 	}
+
+	return r
+}
+
+func (cpu *CPU) nop() {
+}
+
+// TODO: ADD FLAGS AND OUT OF RANGE ARITHMETICS
+func (cpu *CPU) incA() {
+	cpu.registers.A++
+}
+
+// TODO: ADD FLAGS AND OUT OF RANGE ARITHMETICS
+func (cpu *CPU) incPC() {
+	cpu.registers.PC++
+}
+
+func (cpu *CPU) ldR8R8(r1Name, r2Name byte) func() {
+	if r1Name == r2Name {
+		return func() {}
+	}
+
+	r1 := cpu.determineReg(r1Name)
+	r2 := cpu.determineReg(r2Name)
+
+	return func() {
+		*r1 = *r2
+	}
+}
+
+// Requires fetchData as first procedure in instruction
+func (cpu *CPU) ldR8N8(rName byte) func() {
+	r := cpu.determineReg(rName)
 
 	return func() {
 		*r = cpu.registers.temp
