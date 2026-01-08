bc9a78efb357ea6857e39736658771330bf3d59d
kourtnet <itromart@gmail.com>
2026-01-08 09:53:35 +0300

    LD_16bit: Finished LD SP, HL. Tested it

--- internal/cpu/init.go
+++ internal/cpu/init.go
@@ -165,6 +165,9 @@ func (cpu *CPU) init_LD_HL_Inc_Dec() {
 }
 
 func (cpu *CPU) init_LD16() {
+	// LD SP, HL
+	cpu.instructions[0xF9] = []func(){cpu.ld_R16_R16("SP", "HL"), cpu.nop}
+
 	cpu.init_LD_R16_N16()
 }
 
