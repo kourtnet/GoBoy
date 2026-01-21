3eece35a7992ea8cf49fcba3064118d4c0b35740
kourtnet <itromart@gmail.com>
2026-01-21 21:58:32 +0300

    debugger: added registers and flags back

--- internal/debugger/debugger.go
+++ internal/debugger/debugger.go
@@ -11,14 +11,24 @@ import (
 	"github.com/rivo/tview"
 )
 
-var instructionsTitle = "Instructions"
+type regType interface {
+	~byte | ~uint16 | ~bool
+}
+
+var (
+	instructionsTitle = "Instructions"
+	registersTitle    = "Regs"
+	flagsTitle        = "Flg"
+)
 
 const romAddrEnd uint16 = 0x7FFF
 
 type tui struct {
-	app      *tview.Application
-	instList *instructionList
-	topFlex  *tview.Flex
+	app       *tview.Application
+	instList  *instructionList
+	regTable  *tview.Table
+	flagTable *tview.Table
+	topFlex   *tview.Flex
 }
 
 // TODO: mb change to tui instead of deb?
@@ -27,7 +37,6 @@ func newTUI() *tui {
 	tui.app = tview.NewApplication()
 
 	tui.instList = newInstructionList()
-
 	tui.instList.
 		SetHighlightFullLine(true).
 		ShowSecondaryText(false).
@@ -36,9 +45,44 @@ func newTUI() *tui {
 		SetTitleAlign(tview.AlignLeft).
 		SetTitleColor(tcell.ColorGreen)
 
+	tui.regTable = tview.NewTable()
+	tui.regTable.
+		SetSelectable(false, false).
+		SetTitle(registersTitle).
+		SetTitleAlign(tview.AlignCenter).
+		SetTitleColor(tcell.ColorYellow).
+		SetBorder(true)
+
+	tui.regTable.SetCellSimple(0, 0, " a")
+	tui.regTable.SetCellSimple(1, 0, " f")
+	tui.regTable.SetCellSimple(2, 0, " b")
+	tui.regTable.SetCellSimple(3, 0, " c")
+	tui.regTable.SetCellSimple(4, 0, " d")
+	tui.regTable.SetCellSimple(5, 0, " e")
+	tui.regTable.SetCellSimple(6, 0, " h")
+	tui.regTable.SetCellSimple(7, 0, " l")
+	tui.regTable.SetCellSimple(8, 0, " pc")
+	tui.regTable.SetCellSimple(9, 0, " sp")
+
+	tui.flagTable = tview.NewTable()
+	tui.flagTable.
+		SetSelectable(false, false).
+		SetSeparator('=').
+		SetTitle(flagsTitle).
+		SetTitleAlign(tview.AlignCenter).
+		SetTitleColor(tcell.ColorRed).
+		SetBorder(true)
+
+	tui.flagTable.SetCellSimple(0, 0, " z")
+	tui.flagTable.SetCellSimple(1, 0, " n")
+	tui.flagTable.SetCellSimple(2, 0, " h")
+	tui.flagTable.SetCellSimple(3, 0, " c")
+
 	tui.topFlex = tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(tui.instList, 0, 1, true)
+		SetDirection(tview.FlexColumn).
+		AddItem(tui.instList, 0, 1, true).
+		AddItem(tui.regTable, 11, 0, false).
+		AddItem(tui.flagTable, 7, 0, false)
 
 	tui.app.SetRoot(tui.topFlex, true)
 
@@ -74,6 +118,7 @@ func New(cpu *cpu.CPU, b *bus.Bus) (*Debugger, error) {
 	deb := &Debugger{
 		cpu:    cpu,
 		bus:    b,
+		regs:   *cpu.Registers,
 		tui:    newTUI(),
 		doneCh: make(chan struct{}),
 	}
@@ -131,6 +176,58 @@ func (deb *Debugger) loadInstructions() error {
 	return nil
 }
 
+func compareRegs[T regType](table *tview.Table, row int, r1, r2 T) {
+	var cellText string
+	switch any(r2).(type) {
+	case byte:
+		cellText = fmt.Sprintf("%02X", r2)
+	case uint16:
+		cellText = fmt.Sprintf("%04X", r2)
+	case bool:
+		b2 := any(r2).(bool)
+		if b2 {
+			cellText = "1 "
+		} else {
+			cellText = "0 "
+		}
+	}
+
+	cell := table.GetCell(row, 1).SetText(cellText)
+	table.SetCell(row, 1, cell)
+
+	if r1 != r2 {
+		table.GetCell(row, 0).SetTextColor(tcell.ColorRed)
+		table.GetCell(row, 1).SetTextColor(tcell.ColorRed)
+	} else {
+		table.GetCell(row, 0).SetTextColor(tcell.ColorWhite)
+		table.GetCell(row, 1).SetTextColor(tcell.ColorWhite)
+	}
+}
+
+func (deb *Debugger) setRegs() {
+	table := deb.tui.regTable
+	regs := deb.cpu.Registers
+	debRegs := deb.regs
+
+	compareRegs(table, 0, debRegs.A, regs.A)
+	compareRegs(table, 1, debRegs.F, regs.F)
+	compareRegs(table, 2, debRegs.B, regs.B)
+	compareRegs(table, 3, debRegs.C, regs.C)
+	compareRegs(table, 4, debRegs.D, regs.D)
+	compareRegs(table, 5, debRegs.E, regs.E)
+	compareRegs(table, 6, debRegs.H, regs.H)
+	compareRegs(table, 7, debRegs.L, regs.L)
+	compareRegs(table, 8, debRegs.PC(), regs.PC())
+	compareRegs(table, 9, debRegs.SP(), regs.SP())
+
+	table = deb.tui.flagTable
+
+	compareRegs(table, 0, debRegs.FlagZ(), regs.FlagZ())
+	compareRegs(table, 1, debRegs.FlagN(), regs.FlagN())
+	compareRegs(table, 2, debRegs.FlagH(), regs.FlagH())
+	compareRegs(table, 3, debRegs.FlagC(), regs.FlagC())
+}
+
 func (deb *Debugger) stepInst() {
 	for {
 		select {
@@ -153,6 +250,8 @@ func (deb *Debugger) stepInst() {
 
 		if deb.cpu.ReadNewInstruction {
 			deb.tui.instList.setCurrItemByPC(deb.cpu.Registers.PC() - 1)
+			deb.setRegs()
+			deb.regs = *deb.cpu.Registers
 			break
 		}
 	}
@@ -180,6 +279,8 @@ func (deb *Debugger) Run() error {
 	}
 
 	deb.tui.instList.setCurrItemByPC(deb.cpu.Registers.PC() - 1)
+	deb.setRegs()
+	deb.regs = *deb.cpu.Registers
 
 	go func() {
 		deb.err = deb.tui.app.Run()
